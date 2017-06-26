4003245918a9dc4c6c195162f04df09a509f23ba
shangyuzhong <nmdwgll@gmail.com>
2017-06-26 18:19:14 +0800

    重启脚本修改

--- bin/r.sh
+++ bin/r.sh
@@ -1,3 +1,5 @@
-svn up ./luascript
+cd ../../git_stable/bin/luascript/ 
+git pull
+cd ..
 rsync -avz ./luascript shangyz@10.0.253.11:~/bin
 ssh shangyz@10.0.253.11 "cd bin; ./kill.sh;sleep 2; ./start.sh"
